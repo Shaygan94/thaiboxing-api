cbf2e24276ff9e64ac40547c18f9148df189050e
Shaygan Kordestani <shaygan1994@gmail.com>
2025-08-21 17:42:07 +0200

    For test av Heroku måtte fjerne database for å sjekke om hedoku fungerte..

--- src/main/java/com/thaiboxing/thaiboxingapi/repository/CountryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.thaiboxing.thaiboxingapi.repository;
-
-import com.thaiboxing.thaiboxingapi.entity.Country;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CountryRepository extends JpaRepository<Country, String> {
-}
